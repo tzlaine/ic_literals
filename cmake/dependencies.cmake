1d77b1b06f9aa4889ff4949f9b3245693c25b7be
Zach Laine <whatwasthataddress@gmail.com>
2022-11-13 19:05:22 -0600

    Initial add of CMake build skeleton.

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,18 @@
+# Copyright Zach Laine 2022
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+###############################################################################
+# Boost
+###############################################################################
+find_package(Boost 1.74.0 COMPONENTS ${boost_components})
+
+
+###############################################################################
+# GoogleTest
+###############################################################################
+add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.10.0)
+target_compile_definitions(gtest      PUBLIC GTEST_LANG_CXX11=1)
+target_compile_definitions(gtest_main PUBLIC GTEST_LANG_CXX11=1)
+target_include_directories(gtest      INTERFACE $<BUILD_INTERFACE:${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include>)
+target_include_directories(gtest_main INTERFACE $<BUILD_INTERFACE:${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include>)
